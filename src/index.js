6dfb0a40c6f1f8f4ec92da008ebc48c448a9ba78
Nikhil Kumar <nikhilkumarsin2003@gmail.com>
2025-06-20 17:22:45 +0530

    Fix: Corrected Home.js HTML issue and HashRouter basename casing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 // src/index.js
 
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // 1. Import BrowserRouter
+import { HashRouter } from 'react-router-dom';
 import './index.css';
-import Home from './Home';
+import Home from './Home'; // This is your AppLayout component now
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* 2. Wrap your <Home /> component with <BrowserRouter> */}
-    <HashRouter  basename="/S.choraria">
+    {/* Ensure basename matches your GitHub repo name exactly (case-sensitive) */}
+    <HashRouter basename="/S.Choraria"> {/* <-- Fix capitalization here */}
       <Home />
     </HashRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
